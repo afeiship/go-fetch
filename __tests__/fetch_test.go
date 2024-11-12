42ba5e0cff0008301cd1ca58856f1f9278d845c0
afeiship <1290657123@qq.com>
2024-11-12 18:09:05 +0800

    feat: get

--- __tests__/fetch_test.go
+++ __tests__/fetch_test.go
@@ -3,9 +3,26 @@ package fetch
 import (
 	"testing"
 
+	"fmt"
 	"github.com/afeiship/go-fetch"
 )
 
-func TestSayHi(f *testing.T) {
-	go-fetch.SayHi()
+func TestGet(f *testing.T) {
+	res, err := fetch.Get("https://www.httpbin.org/get", &fetch.Config{
+		Headers: map[string]string{
+			"X-Custom-Header": "aric",
+		},
+		Params: map[string]string{
+			"param1": "value1",
+		},
+		Query: map[string]string{
+			"query1": "value1",
+			"query2": "value2",
+		},
+	})
+
+	if err!= nil {
+		f.Error(err)
+	}
+	fmt.Println(json.Marshal(res.Body))
 }
--- fetch.go
+++ fetch.go
@@ -3,8 +3,14 @@ package fetch
 // https://github.com/go-zoox/fetch
 
 import (
+	"bytes"
 	"fmt"
+	"io"
+	"net/http"
+	"net/url"
+	"strings"
 )
+
 
 type Headers map[string]string
 type Query map[string]string
@@ -20,13 +26,56 @@ type Config struct {
 	Body    Body
 }
 
-func SayHi() {
-	fmt.Println("Hi from go-fetch")
-}
+func Get(baseURL string, config *Config) (*http.Response, error) {
+	// Replace params in the URL template
+	for key, value := range config.Params {
+		placeholder := fmt.Sprintf("{%s}", key)
+		baseURL = strings.ReplaceAll(baseURL, placeholder, value)
+	}
 
-func Get(config Config) (string, error) {
-	// TODO: implement Get method
-	return "", nil
+	// Parse the base URL to work with queries
+	u, err := url.Parse(baseURL)
+	if err != nil {
+		return nil, fmt.Errorf("invalid URL: %v", err)
+	}
+
+	// Add query parameters
+	q := u.Query()
+	for key, value := range config.Query {
+		q.Set(key, value)
+	}
+	u.RawQuery = q.Encode()
+
+	// Create the request
+	req, err := http.NewRequest(config.Method, u.String(), nil)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create request: %v", err)
+	}
+
+	// Set headers
+	for key, value := range config.Headers {
+		req.Header.Set(key, value)
+	}
+
+	// Add body if specified and method is not GET
+	if config.Body != nil && config.Method != "GET" {
+		var body io.Reader
+		switch b := config.Body.(type) {
+		case string:
+			body = strings.NewReader(b)
+		case []byte:
+			body = bytes.NewReader(b)
+		case io.Reader:
+			body = b
+		default:
+			return nil, fmt.Errorf("unsupported body type")
+		}
+		req.Body = io.NopCloser(body)
+	}
+
+	// Send the request
+	client := &http.Client{}
+	return client.Do(req)
 }
 
 func Post(config Config) (string, error) {
